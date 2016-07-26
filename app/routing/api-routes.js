52943393094044308b17e448991ca7d000c8f574
kellymersereau <kelly.mersereau@gmail.com>
2016-07-26 17:30:56 -0400

    updated some things

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -4,7 +4,7 @@
 // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 
 
-var friendsData = require('../data/friends.js');
+var friends = require('../data/friends.js');
 
 module.exports = function(app){
 	app.get('/api/friends', function(req, res){
